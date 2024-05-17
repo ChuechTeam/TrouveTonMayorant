460d68e51d322089bc79e3c500078ef9f5b97441
Arthur-N42 <arthur.nz.ju@gmail.com>
2024-05-17 22:11:03 +0200

    on ne peut plus trouver les gens qui nous ont bloqué

--- src/newrecup.php
+++ src/newrecup.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once "modules/userDB.php";
+require_once "modules/user.php";
+require_once "modules/userSession.php";
 require_once "./member-area/_profileCard.php";
 require "./modules/url.php";
 
@@ -16,7 +18,9 @@ foreach(UserDB\query() as $u){
     $a = (new DateTime($u["bdate"]))->diff(new DateTime())->y;
     if ((empty($g) || in_array($u["gender"],$g)) && 
         ($f == null || $u["user_smoke"]==$f) &&
-        ($a <= $a_max && $a >=$a_min )
+        ($a <= $a_max && $a >=$a_min) &&// age autour de celui du user
+        (User\blockStatus(userSession\loggedUserId(), $u["id"]) != 1 )
+        
         ) {
         if ($first) {
             echo '<div class="search-results">';
